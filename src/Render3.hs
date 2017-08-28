e424186c71a753fbd3f0b34d8bdd03e9f848efe8
felixgb <felixgbowman@gmail.com>
2017-08-28 23:30:41 +0100

    dao code is now in Model.hs

--- src/Render3.hs
+++ src/Render3.hs
@@ -1,10 +1,11 @@
 module Render3 where
 
+import Control.Monad.Reader
 import Control.Monad (when, unless, forever)
 import Control.Exception
 import System.Exit
 import Foreign
-import Foreign.C.String (newCAStringLen, newCString)
+import Foreign.C.String (CString, newCAStringLen, newCString)
 
 import qualified Graphics.UI.GLFW as GLFW
 import Graphics.GL.Core33
@@ -13,6 +14,7 @@ import Linear
 
 import qualified Util.Common as U
 import qualified Util.Shaders as U
+import qualified Util.Model as U
 
 winWidth = 800
 winHeight = 600
@@ -37,73 +39,57 @@ initWindow = do
     Just window -> return window
     Nothing     -> throwIO U.WindowCreationError
 
-setCallbacks :: GLFW.Window -> IO (GLuint, GLuint)
-setCallbacks window = do
-  putStrLn "called"
+data RenderData = RenderData
+  { _models          :: [U.VaoModel]
+  , _shaderProgram   :: GLuint
+  , _transformString :: CString
+  , _transP          :: Ptr (V4 (V4 GLfloat))
+  }
+
+colors = [
+    1.0 , 0.0 , 0.0 , -- Top Right
+    0.0 , 0.0 , 1.0 , -- Bottom Right
+    0.0 , 1.0 , 0.0 , -- Bottom Left
+    1.0 , 1.0 , 0.0   -- Top Left
+  ] :: [GLfloat]
+
+verticies = [
+    -- positions
+    0.5  , 0.5  , 0.0 , -- Top Right
+    0.5  , -0.5 , 0.0 , -- Bottom Right
+    -0.5 , -0.5 , 0.0 , -- Bottom Left
+    -0.5 , 0.5  , 0.0   -- Top Left
+  ] :: [GLfloat]
+
+indices = [  -- Note that we start from 0!
+  0, 1, 3, -- First Triangle
+  1, 2, 3  -- Second Triangle
+  ] :: [GLuint]
+
+setupWindow :: GLFW.Window -> IO ()
+setupWindow window = do
   GLFW.setKeyCallback window (Just keyCallback)
   GLFW.makeContextCurrent (Just window)
   (x, y) <- GLFW.getFramebufferSize window
   glViewport 0 0 (fromIntegral x) (fromIntegral y)
 
+
+setupData :: IO RenderData
+setupData = do
   shaderProgram <- U.initShaders
     [ (GL_VERTEX_SHADER, "src/glsl/vertex.shader")
     , (GL_FRAGMENT_SHADER, "src/glsl/fragment.shader")
     ]
-
-  let verticies = [
-        0.5,  0.5, 0.0,  -- Top Right
-        0.5, -0.5, 0.0,  -- Bottom Right
-        -0.5, -0.5, 0.0, -- Bottom Left
-        -0.5,  0.5, 0.0  -- Top Left
-          ] :: [GLfloat]
-  let verticesSize = fromIntegral $ sizeOf (0.0 :: GLfloat) * (length verticies)
-  verticesP <- newArray verticies
-
-  let indices = [  -- Note that we start from 0!
-          0, 1, 3, -- First Triangle
-          1, 2, 3  -- Second Triangle
-          ] :: [GLuint]
-  let indicesSize = fromIntegral $ sizeOf (0.0 :: GLfloat) * (length indices)
-  indicesP <- newArray indices
-
-  -- setup vertex array object
-  vaoP <- malloc
-  glGenVertexArrays 1 vaoP
-  vao <- peek vaoP
-  glBindVertexArray vao
-
-  -- setup vertex buffer object and send it data
-  vboP <- malloc
-  glGenBuffers 1 vboP
-  vbo <- peek vboP
-  glBindBuffer GL_ARRAY_BUFFER vbo
-  glBufferData GL_ARRAY_BUFFER verticesSize (castPtr verticesP) GL_STATIC_DRAW
-
-  eboP <- malloc
-  glGenBuffers 1 eboP
-  ebo <- peek eboP
-
-  glBindBuffer GL_ELEMENT_ARRAY_BUFFER ebo
-  glBufferData GL_ELEMENT_ARRAY_BUFFER indicesSize (castPtr indicesP) GL_STATIC_DRAW
-
-  let floatSize = (fromIntegral $ sizeOf (0.0 :: GLfloat)) :: GLsizei
-  -- position attribute
-  glVertexAttribPointer 0 3 GL_FLOAT GL_FALSE (3 * floatSize) nullPtr
-  glEnableVertexAttribArray 0
-
-  -- color attribute, where do these come from
-  let threeFloatOffset = castPtr $ plusPtr nullPtr (fromIntegral $ 3 * floatSize)
-  glVertexAttribPointer 1 3 GL_FLOAT GL_FALSE (2 * floatSize) threeFloatOffset
-  glEnableVertexAttribArray 1
-
-  glBindVertexArray 0
-  return (vao, shaderProgram)
+  model     <- U.loadToVao $ U.Object verticies colors indices
+  transform <- newCString "transform"
+  transP    <- malloc
+  return $ RenderData [model] shaderProgram transform transP
 
 terminate :: IO ()
 terminate = GLFW.terminate >> exitSuccess
 
-displayLoop :: GLFW.Window -> GLuint -> GLuint -> IO ()
-displayLoop window vao shaderProgram = forever $ do
+displayLoop :: GLFW.Window -> RenderData -> IO ()
+displayLoop window renderData = forever $ do
   shouldContinue <- not <$> GLFW.windowShouldClose window
   unless shouldContinue terminate
 
@@ -111,22 +97,21 @@ displayLoop window vao shaderProgram = forever $ do
   glClearColor 0.2 0.3 0.3 1.0
   glClear GL_COLOR_BUFFER_BIT
 
-  transform <- newCString "transform"
-
-  transP <- malloc
   timeValue <- maybe 0 realToFrac <$> GLFW.getTime
   let rotQ = axisAngle (V3 (0 :: GLfloat) 0 1) timeValue
   let rotM33 = fromQuaternion rotQ
   let rotM33' = rotM33 !!* 0.5
-  let transformationMatrix = mkTransformationMat rotM33' (V3 0.5 (-0.5) 0)
-  poke transP (transpose transformationMatrix)
-  transformLoc <- glGetUniformLocation shaderProgram transform
-  glUniformMatrix4fv transformLoc 1 GL_FALSE (castPtr transP)
+  let transformationMatrix = mkTransformationMat rotM33' (V3 0.0 (-0.0) 0)
+  poke (_transP renderData) (transpose transformationMatrix)
+  transformLoc <- glGetUniformLocation (_shaderProgram renderData) (_transformString renderData)
+  glUniformMatrix4fv transformLoc 1 GL_FALSE (castPtr (_transP renderData))
 
-  glUseProgram shaderProgram
-  glBindVertexArray vao
-  glDrawElements GL_TRIANGLES 6 GL_UNSIGNED_INT nullPtr
-  glBindVertexArray 0
+  glUseProgram (_shaderProgram renderData)
+
+  forM_ (_models renderData) $ \(U.VaoModel id numVertices) -> do
+    glBindVertexArray id
+    glDrawElements GL_TRIANGLES numVertices GL_UNSIGNED_INT nullPtr
+    glBindVertexArray 0
 
   GLFW.swapBuffers window
 
@@ -137,5 +122,6 @@ go = catch body handler
     handler ex = print ex >> GLFW.terminate
     body = do
       window <- initWindow
-      (vao, shaderProgram) <- setCallbacks window
-      displayLoop window vao shaderProgram
+      setupWindow window
+      renderData <- setupData
+      displayLoop window renderData
---
+++ src/Util/Model.hs
@@ -0,0 +1,84 @@
+module Util.Model where
+
+import Data.List
+import Foreign
+
+import Graphics.GL.Core33
+import Graphics.GL.Types
+
+data Object = Object
+  { vertices :: [GLfloat]
+  , colors   :: [GLfloat]
+  , indices  :: [GLuint]
+  }
+
+data VaoModel = VaoModel
+  { _vaoID       :: GLuint
+  , _numVertices :: GLsizei
+  }
+
+-- | A VAO stores data about a 3d model. They have a number of slots called
+-- Attribute Lists. You store different sets of data into these attribute
+-- lists. ex: 0: vertex positions, 1: vertex colors...
+--
+-- These data sets are called VBOs. A VBO is an array of any kind of data,
+-- positions, normals, etc
+--
+-- Each VAO has a unique ID, so you can reference it any time sing its ID.  You
+-- can use the id of a VAO to render an object
+loadToVao :: Object -> IO VaoModel
+loadToVao (Object vs cs is) = do
+  verticesP <- newArray interwoven
+  indicesP <- newArray is
+
+  -- setup vertex array object
+  vaoP <- malloc
+
+  -- 1 is the number of vertex array object names to generate
+  -- vaoP is the array where generate vertex array object names are to be stored
+  glGenVertexArrays 1 vaoP
+  vao <- peek vaoP
+
+  -- bind the VAO id so we are changing this one
+  glBindVertexArray vao
+
+  -- setup vertex buffer object and send it data
+  vboP <- malloc
+  glGenBuffers 1 vboP
+  vbo <- peek vboP
+  glBindBuffer GL_ARRAY_BUFFER vbo
+  glBufferData GL_ARRAY_BUFFER verticesSize (castPtr verticesP) GL_STATIC_DRAW
+
+  -- set up an element buffer and send it data
+  eboP <- malloc
+  glGenBuffers 1 eboP
+  ebo <- peek eboP
+  glBindBuffer GL_ELEMENT_ARRAY_BUFFER ebo
+  glBufferData GL_ELEMENT_ARRAY_BUFFER indicesSize (castPtr indicesP) GL_STATIC_DRAW
+
+  -- position attribute is in 0
+  -- 6 is the number from the start of a vertex to the start of the next
+  -- vertex, eg x y z r g b x
+  glVertexAttribPointer 0 3 GL_FLOAT GL_FALSE (6 * floatSize) nullPtr
+  glEnableVertexAttribArray 0
+
+  -- color attribute is in 1
+  glVertexAttribPointer 1 3 GL_FLOAT GL_FALSE (6 * floatSize) threeFloatOffset
+  glEnableVertexAttribArray 1
+
+  -- unbind this VAO
+  glBindVertexArray 0
+  return $ VaoModel vao (fromIntegral $ length is)
+  where
+    floatSize        = (fromIntegral $ sizeOf (0.0 :: GLfloat)) :: GLsizei
+    -- three float offset because colors start after x y z
+    threeFloatOffset = castPtr $ plusPtr nullPtr (fromIntegral $ 3 * floatSize)
+    verticesSize     = fromIntegral $ sizeOf (0.0 :: GLfloat) * (length interwoven)
+    indicesSize      = fromIntegral $ sizeOf (0 :: GLuint) * (length is)
+    interwoven       = interweave vs cs
+
+interweave [] [] = []
+interweave as xs = ah ++ xh ++ interweave at xt
+  where
+    (ah, at) = splitAt 3 as
+    (xh, xt) = splitAt 3 xs
